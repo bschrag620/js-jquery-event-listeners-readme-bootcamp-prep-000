3dc184454e4bfeeeff80669a990938f29432d29b
Brad Schrag <brad.schrag@gmail.com>
2018-07-13 17:24:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,7 +9,7 @@ function getIt() {
 
 function frameIt() {
   $('img').on('load', function() {
-    const imgFrame = getElementBy
+    const imgFrame = document.getElementByTagName
     return
   })
 }
